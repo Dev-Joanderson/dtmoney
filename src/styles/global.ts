77a0b3964f0ebf0fa29d5ed1a2c2ee3f71156f94
Joanderson Trajano De Araujo <72756013+joanderson00@users.noreply.github.com>
2022-04-25 22:51:43 -0300

    :tada: Challenge Finalized

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle `
     :root{
-        --backgroud:#F0F2F5;
+        --background:#F0F2F5;
         --red:#E62E4D;
         --blue:#5429CC;
 
@@ -14,7 +14,6 @@ export const GlobalStyle = createGlobalStyle `
         --text-title:#363F5F;
         --text-body:#969CB3;
         
-        --backgroud:#f0f2f5;
         --shape:#FFFFFF;
 
 
@@ -55,6 +54,31 @@ export const GlobalStyle = createGlobalStyle `
         opacity:0.6;
         cursor:not-allowed;
 
+    }
+
+    .react-modal-overlay {
+        background: rgba(0, 0, 0, 0.5);
+
+        position:fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+       
+    }
+    .react-modal-content {
+        
+        width: 100%;
+        max-width: 576px;
+        
+        background: var(--background);
+        padding: 3rem;
+        position: relative;
+        border-radius: 0.25rem;
     }
 
 
